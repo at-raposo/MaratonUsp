c36734fc854eab3a2b03b039bfb2b72417866ff8
Raposo <92121267+at-raposo@users.noreply.github.com>
2024-03-15 15:03:28 -0300

    Update and rename Condicionais e Loop to Condicionais e Loop.cpp
    Aula para ver como funciona as condicionais

---
+++ Aula 2 - BixeCamp/Condicionais e Loop.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> //biblioteca da USP?
+using namespace std;
+
+int main(){
+
+int main(){
+	int x;
+	cin >> x;
+
+	if(x > 0){ 
+		cout << "positivo" == endl;
+}
+    else if(x< 0){
+    	cout << "negativo" << endl;
+    }
+    else{
+    	cout << "nulo" << endl;
+    }
+
+}
+
